0c27cd97425d458f466f27bb1eca21d87e6de5ff
Dio Lantief Widoyoko <dio_lantief21@outlook.com>
2023-06-04 05:46:51 +0700

    Update base.blade.php

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -29,6 +29,11 @@
         main {
             flex: 1 0 auto;
         }
+
+        /* general progress */
+        .progress.general {
+            margin: 0 !important;
+        }
     </style>
 
     @include('base.component.css')
@@ -41,16 +46,42 @@
 </head>
 
 <body>
+    <div id="progress-general" class="progress general">
+        <div class="indeterminate"></div>
+    </div>
     @yield('main')
     @yield('fab')
     @include('base.component.js')
     @yield('extra-base-js')
     <script>
+        let progressGeneral = document.getElementById('progress-general')
+
+        function loading(isLoading) {
+            if (progressGeneral.classList.contains('hide')) {
+                progressGeneral.classList.remove('hide')
+                return
+            }
+            progressGeneral.classList.add('hide')
+        }
+
         M.AutoInit();
         $.ajaxSetup({
             headers: {
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            beforeSend: function(xhr) {
+                loading(true)
+            },
+            afterSend: function(xhr) {
+                loading(false)
+            },
+            done: function(xhr) {
+                loading(false)
             }
+        });
+
+        document.addEventListener("DOMContentLoaded", function() {
+            loading(false)
         });
     </script>
     @yield('extra-js')
