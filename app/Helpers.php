bc93f3408a397fd78ccc7b9e2b8759495c70c46b
Dio Lantief Widoyoko <dio_lantief21@outlook.com>
2023-06-14 23:36:25 +0700

    Update Helpers.php

--- app/Helpers.php
+++ app/Helpers.php
@@ -1,1 +1,18 @@
 <?php
+
+if(!function_exists('assets')) {
+    function assets(string $path) {
+        return URL::to('assets/'.$path);
+    }
+}
+
+if(!function_exists('css')) {
+    function css(string $path) {
+        return assets('css/'.$path);
+    }
+}
+if(!function_exists('js')) {
+    function js(string $path) {
+        return assets('js/'.$path);
+    }
+}
