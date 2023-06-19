70b2990219e40c71c317c263cecfeadc949bafe3
Dio Lantief Widoyoko <dio_lantief21@outlook.com>
2023-06-20 02:31:53 +0700

    Update index.blade.php

--- resources/views/public/home/index.blade.php
+++ resources/views/public/home/index.blade.php
@@ -1,8 +1,9 @@
 @extends('base.public')
 
 @section('extra-css')
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <style>
-        #bgVideo {
+        video {
             z-index: -1;
             position: fixed;
             right: 0;
@@ -13,63 +14,37 @@
 
         /* Add some content at the bottom of the video/page */
         .content {
+            text-align: center;
             position: fixed;
-            bottom: 0;
+            top: 40%;
+            transform: translateY(-50%);
             background: rgba(0, 0, 0, 0.5);
             color: #f1f1f1;
             width: 100%;
             padding: 20px;
-        }
-
-        /* Style the button used to pause/play the video */
-        #myBtn {
-            width: 200px;
-            font-size: 18px;
-            padding: 10px;
-            border: none;
-            background: #000;
-            color: #fff;
-            cursor: pointer;
-        }
-
-        #myBtn:hover {
-            background: #ddd;
-            color: black;
         }
     </style>
 @endsection
 
 @section('content')
     <!-- The video -->
-    <video autoplay muted loop id="bgVideo">
-        <source src="http://localhost:8000/assets/videos/bg.mp4" type="video/mp4">
+    <video autoplay muted loop>
+        <source src="{{ assets('videos/bg.mp4') }}" type="video/mp4">
     </video>
     <!-- Optional: some overlay text to describe the video -->
-    <div class="content">
+    <div class="content" data-aos="fade-up">
         <h1>{{ env('APP_NAME', 'Laravel') }}</h1>
-        <?= \Diolan12\LoremIpsum::instance()->wrap()->sentences(4) ?>
-        <!-- Use a button to pause/play the video with JavaScript -->
-        <button id="myBtn" onclick="toggleVideo()">Pause</button>
+        <?= \Diolan12\LoremIpsum::instance()
+                ->wrap()
+                ->sentences(4) ?>
+        <a href="!#" class="btn waves-light">Explore</a>
+        <a href="!#" class="btn secondary-color">Book Now</a>
     </div>
 @endsection
 
 @section('extra-js')
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script>
-        // Get the video
-        var video = document.getElementById("bgVideo");
-
-        // Get the button
-        var btn = document.getElementById("myBtn");
-
-        // Pause and play the video, and change the button text
-        function toggleVideo() {
-            if (video.paused) {
-                video.play();
-                btn.innerHTML = "Pause";
-            } else {
-                video.pause();
-                btn.innerHTML = "Play";
-            }
-        }
+        AOS.init();
     </script>
 @endsection
