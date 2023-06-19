ee73f07ce6aa2e6887770020f6cf1a8362ff02bf
Dio Lantief Widoyoko <dio_lantief21@outlook.com>
2023-06-20 01:07:30 +0700

    Update index.blade.php

--- resources/views/dashboard/stats/index.blade.php
+++ resources/views/dashboard/stats/index.blade.php
@@ -1,48 +1,39 @@
 @extends('base.dashboard')
 @section('extra-css')
     <style>
-        .row .col {
-            margin: 6px;
-        }
     </style>
 @endsection
 @section('content')
     <div class="row">
-        <div class="col s12 m6">
+        <div class="col s12 m12 l12 xl6">
             <div class="card-panel">
                 <div>
                     <canvas id="chart"></canvas>
                 </div>
             </div>
         </div>
-        <div class="col s6 m3">
+        <div class="col s6 m6 l6 xl3">
             <div class="card-panel">
                 <div>
                     <canvas id="doughnut"></canvas>
                 </div>
             </div>
         </div>
-        <div class="col s6 m3">
+        <div class="col s6 m6 l6 xl3">
             <div class="card-panel">
                 <div>
                     <canvas id="radar"></canvas>
                 </div>
             </div>
         </div>
-        <div class="col s12 m3">
+        <div class="col s6">
             <div class="card-panel teal">
-                <span class="white-text">I am a very simple card. I am good at containing small bits of information.
-                    I am convenient because I require little markup to use effectively. I am similar to what is called a
-                    panel in other frameworks.
-                </span>
+                <span class="white-text">{{ \Diolan12\LoremIpsum::instance()->sentences(7) }}</span>
             </div>
         </div>
-        <div class="col s12 m3">
+        <div class="col s6">
             <div class="card-panel">
-                <span class="white-text">I am a very simple card. I am good at containing small bits of information.
-                    I am convenient because I require little markup to use effectively. I am similar to what is called a
-                    panel in other frameworks.
-                </span>
+                <span class="white-text">{{ \Diolan12\LoremIpsum::instance()->sentences(7) }}</span>
             </div>
         </div>
     </div>
