c74b398555b1a009be066fca2de49839e9836b33
Dio Lantief Widoyoko <dio_lantief21@outlook.com>
2023-06-20 10:28:10 +0700

    Update JsonStd.php

--- app/Helpers/JsonStd.php
+++ app/Helpers/JsonStd.php
@@ -1,43 +1,50 @@
 <?php
+
 namespace App\Helpers;
 
-class JsonStd {
-
+class Std
+{
+    public $status;
+    public $message;
+    public $payload;
+    public function __construct($status, $message, $payload)
+    {
+        $this->status = $status;
+        $this->message = $message;
+        $this->payload = $payload;
+        return $this;
+    }
+    public static function create($status, $message, $payload)
+    {
+        return new Std($status, $message, $payload);
+    }
+}
+class JsonStd
+{
     public const SUCCESS = 'SUCCESS';
     public const ERROR = 'ERROR';
-    private static $STD = [
-        'status' => null,
-        'message' => null,
-        'payload' => null
-    ];
 
-    public static function success($payload, $message = null, $code = 200) {
-        $json = self::$STD;
-        $json['status'] = self::SUCCESS;
-        $json['message'] = $message;
-        $json['payload'] = $payload;
-        return response()->json($json, $code);
+    public static function success($payload, $message = null, $code = 200)
+    {
+        $std = Std::create(self::SUCCESS, $message, $payload);
+        return response()->json($std, $code);
     }
 
-    public static function successMsg(string $message, $code = 200) {
-        $json = self::$STD;
-        $json['status'] = self::SUCCESS;
-        $json['message'] = $message;
-        return response()->json($json, $code);
+    public static function successMsg(string $message, $code = 200)
+    {
+        $std = Std::create(self::SUCCESS, $message, null);
+        return response()->json($std, $code);
     }
 
-    public static function error($payload, $message = null, $code = 400) {
-        $json = self::$STD;
-        $json['status'] = self::ERROR;
-        $json['message'] = $message;
-        $json['payload'] = $payload;
-        return response()->json($json, $code);
+    public static function error($payload, $message = null, $code = 400)
+    {
+        $std = Std::create(self::ERROR, $message, $payload);
+        return response()->json($std, $code);
     }
 
-    public static function errorMsg(string $message = null, $code = 400) {
-        $json = self::$STD;
-        $json['status'] = self::ERROR;
-        $json['message'] = $message;
-        return response()->json($json, $code);
+    public static function errorMsg(string $message = null, $code = 400)
+    {
+        $std = Std::create(self::ERROR, $message, null);
+        return response()->json($std, $code);
     }
 }
